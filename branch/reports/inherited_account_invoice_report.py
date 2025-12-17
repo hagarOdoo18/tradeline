54414a7a7bb6cdfca2860ef650eec5d5b979d669
ezzat <e.mosaad@ejad.sa>
2025-12-17 23:44:52 +0200

    upload

--- branch/reports/inherited_account_invoice_report.py
+++ branch/reports/inherited_account_invoice_report.py
@@ -7,7 +7,20 @@ class AccountInvoiceReport(models.Model):
 
     branch_id = fields.Many2one('res.branch')
 
-    def _select(self) -> SQL:
+    reference_number = fields.Char(
+        string='Reference Number',
+        required=False)
+    preferred_payment_method_line_id = fields.Many2one(
+        string="Payment Method",
+        comodel_name='account.payment.method.line',
+        store=True,
+        readonly=False,
+    )
+
+    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist')
+
+
+def _select(self) -> SQL:
         return SQL("%s, move.branch_id AS branch_id, move.reference_number AS reference_number,move.preferred_payment_method_line_id as preferred_payment_method_line_id,move.pricelist_id as pricelist_id",
                    super()._select())
 
