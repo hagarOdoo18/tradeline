a7c36b54fab513e0381324a2f19d9b3dabffcf32
ezzat <e.mosaad@ejad.sa>
2025-12-31 03:31:58 +0200

    upload

--- base_tradeline/models/company.py
+++ base_tradeline/models/company.py
@@ -11,8 +11,39 @@ except ImportError:
     num2words = None
 class Currency(models.Model):
     _inherit = "res.currency"
+    def en_amount_to_text(self, amount):
+        self.ensure_one()
 
-    def amount_to_text(self, amount):
+        def _num2words(number, lang):
+            try:
+                return num2words(number, lang=lang).title()
+            except NotImplementedError:
+                return num2words(number, lang='en').title()
+
+        if num2words is None:
+            logging.getLogger(__name__).warning("The library 'num2words' is missing, cannot render textual amounts.")
+            return ""
+
+        integral, _sep, fractional = f"{amount:.{self.decimal_places}f}".partition('.')
+        integer_value = int(integral)
+        lang = tools.get_lang(self.env)
+        if self.is_zero(amount - integer_value):
+            return _(
+                '%(integral_amount)s %(currency_unit)s',
+                integral_amount=_num2words(integer_value, lang=lang.iso_code),
+                currency_unit=self.currency_unit_label,
+            )
+        else:
+            return _(
+                '%(integral_amount)s %(currency_unit)s and %(fractional_amount)s %(currency_subunit)s',
+                integral_amount=_num2words(integer_value, lang=lang.iso_code),
+                currency_unit=self.currency_unit_label,
+                fractional_amount=_num2words(int(fractional or 0), lang=lang.iso_code),
+                currency_subunit=self.currency_subunit_label,
+            )
+
+
+def amount_to_text(self, amount):
         self.ensure_one()
 
         def _num2words(number, lang):
