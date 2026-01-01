d8147b6f51e8df9a1bf3302c2025bbf282ed7d57
ezzat <e.mosaad@ejad.sa>
2026-01-02 00:27:17 +0200

    upload

--- branch/models/inherited_account_payment.py
+++ branch/models/inherited_account_payment.py
@@ -105,7 +105,7 @@ class AccountPayment(models.Model):
             else:
                 rec.branch_id=rec.move_id.branch_id.id
 
-    branch_id = fields.Many2one('res.branch',store=True,readonly=True,compute="compute_branches")
+    branch_id = fields.Many2one('res.branch',readonly=True,compute="compute_branches")
 
     # @api.onchange('branch_id')
     # def _onchange_branch_id(self):
