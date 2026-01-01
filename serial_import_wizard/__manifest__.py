ae556e62a15568c89393912698247a5858040b89
ezzat <e.mosaad@ejad.sa>
2026-01-01 12:49:50 +0200

    upload

---
+++ serial_import_wizard/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Serial Import Wizard',
+    'version': '1.0',
+    'summary': 'Import Serials from Wizard with custom naming',
+    'category': 'Inventory',
+    'depends': ['stock'],
+    'data': [
+        'views/serial_import_wizard_view.xml',
+    ],
+    'installable': True,
+}
---
+++ serial_import_wizard/wizard/serial_import_wizard.py
@@ -0,0 +1,26 @@
+import base64
+import xlrd
+from odoo import models, fields
+from datetime import datetime
+
+class SerialImportWizard(models.TransientModel):
+    _name = 'serial.import.wizard'
+    _description = 'Serial Import Wizard'
+
+    file = fields.Binary(string="Excel File", required=True)
+    filename = fields.Char()
+    product_id = fields.Many2one('product.product', required=True)
+
+    def action_import(self):
+        book = xlrd.open_workbook(
+            file_contents=base64.b64decode(self.file)
+        )
+        sheet = book.sheet_by_index(0)
+
+        year = datetime.now().year
+
+        for row in range(1, sheet.nrows):
+            serial = str(sheet.cell(row, 0).value).strip()
+            if serial:
+               old= self.env['stock.lot'].search[('name','=',serial)]
+               old.name = serial+'/'
