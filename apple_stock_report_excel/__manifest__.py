e7e9f521c1776bd9d6c529e523f80a375c7a2e22
ezzat <e.mosaad@ejad.sa>
2026-01-04 19:14:39 +0200

    upload

--- apple_stock_report_excel/__manifest__.py
+++ apple_stock_report_excel/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Apple Stock Report Wizard",
     "version": "1.0",
-    "depends": ["stock"],
+    "depends": ["stock",'stock_user_location_domain'],
     "data": [
         "security/ir.model.access.csv",
         "wizard/apple_stock_report_wizard.xml",
