a61d25d5b94fe7d20fe8c6b6f91ee1ac271f2496
ezzat <e.mosaad@ejad.sa>
2026-01-04 20:49:09 +0200

    upload

--- branch/models/inherited_stock_move.py
+++ branch/models/inherited_stock_move.py
@@ -146,55 +146,90 @@ class StockMove(models.Model):
                         return picking
         return False
 
-    def _assign_picking(self):
-        """ Try to assign the moves to an existing picking that has not been
-        reserved yet and has the same procurement group, locations and picking
-        type (moves should already have them identical). Otherwise, create a new
-        picking to assign them to. """
-        """ Try to assign the moves to an existing picking that has not been
-              reserved yet and has the same procurement group, locations and picking
-              type (moves should already have them identical). Otherwise, create a new
-              picking to assign them to. """
-        Picking = self.env['stock.picking']
-        for move in self:
-            recompute = False
-            pickings = move._search_picking_for_assignation()
-            picking = self.check_picking(pickings, move)
-            if picking:
-                if len(picking) == 1:
-                    picking.write({
-                        'partner_id': False,
-                        'origin': False,
-                    })
-                    move.write({'picking_id': picking.id})
-                    move._assign_picking_post_process(new=picking)
-                else:
-                    move.write({'picking_id': picking[1].id})
-                    move._assign_picking_post_process(new=picking)
-            else:
-                recompute = True
-                old_picking = self.env['stock.picking'].search([('name', '=', move.origin)])
 
-                if old_picking and  picking:
-                    if old_picking.note and old_picking.origin:
-                        picking.note = old_picking.note
-                        picking.note += ' / ' + old_picking.origin
-                    elif old_picking.note:
-                        picking.note = old_picking.note
-                    else:
-                        picking.note = old_picking.origin
-                else:
-                    picking = Picking.create(move._get_new_picking_values())
+    # def _assign_picking(self):
+    #     """ Try to assign the moves to an existing picking that has not been
+    #     reserved yet and has the same procurement group, locations and picking
+    #     type (moves should already have them identical). Otherwise, create a new
+    #     picking to assign them to. """
+    #     Picking = self.env['stock.picking']
+    #     grouped_moves = groupby(self, key=lambda m: m._key_assign_picking())
+    #     for group, moves in grouped_moves:
+    #         moves = self.env['stock.move'].concat(*moves)
+    #         new_picking = False
+    #         # Could pass the arguments contained in group but they are the same
+    #         # for each move that why moves[0] is acceptable
+    #         picking = moves[0]._search_picking_for_assignation()
+    #         if picking:
+    #             # If a picking is found, we'll append `move` to its move list and thus its
+    #             # `partner_id` and `ref` field will refer to multiple records. In this
+    #             # case, we chose to wipe them.
+    #             vals = moves._assign_picking_values(picking)
+    #             if vals:
+    #                 picking.write(vals)
+    #         else:
+    #             # Don't create picking for negative moves since they will be
+    #             # reverse and assign to another picking
+    #             moves = moves.filtered(lambda m: float_compare(m.product_uom_qty, 0.0, precision_rounding=m.product_uom.rounding) >= 0)
+    #             if not moves:
+    #                 continue
+    #             new_picking = True
+    #             picking = Picking.create(moves._get_new_picking_values())
+    #
+    #         moves.write({'picking_id': picking.id})
+    #         moves._assign_picking_post_process(new=new_picking)
+    #     return True
 
-                move.write({'picking_id': picking.id})
-                move._assign_picking_post_process(new=picking)
-            # If this method is called in batch by a write on a one2many and
-            # at some point had to create a picking, some next iterations could
-            # try to find back the created picking. As we look for it by searching
-            # on some computed fields, we have to force a recompute, else the
-            # record won't be found.
 
-        return True
+    # def _assign_picking(self):
+    #     """ Try to assign the moves to an existing picking that has not been
+    #     reserved yet and has the same procurement group, locations and picking
+    #     type (moves should already have them identical). Otherwise, create a new
+    #     picking to assign them to. """
+    #     """ Try to assign the moves to an existing picking that has not been
+    #           reserved yet and has the same procurement group, locations and picking
+    #           type (moves should already have them identical). Otherwise, create a new
+    #           picking to assign them to. """
+    #     Picking = self.env['stock.picking']
+    #     for move in self:
+    #         recompute = False
+    #         pickings = move._search_picking_for_assignation()
+    #         picking = self.check_picking(pickings, move)
+    #         if picking:
+    #             if len(picking) == 1:
+    #                 picking.write({
+    #                     'partner_id': False,
+    #                     'origin': False,
+    #                 })
+    #                 move.write({'picking_id': picking.id})
+    #                 move._assign_picking_post_process(new=picking)
+    #             else:
+    #                 move.write({'picking_id': picking[1].id})
+    #                 move._assign_picking_post_process(new=picking)
+    #         else:
+    #             recompute = True
+    #             old_picking = self.env['stock.picking'].search([('name', '=', move.origin)])
+    #
+    #             if old_picking and  picking:
+    #                 if old_picking.note and old_picking.origin:
+    #                     picking.note = old_picking.note
+    #                     picking.note += ' / ' + old_picking.origin
+    #                 elif old_picking.note:
+    #                     picking.note = old_picking.note
+    #                 else:
+    #                     picking.note = old_picking.origin
+    #             else:
+    #                 picking = Picking.create(move._get_new_picking_values())
+    #
+    #             move.write({'picking_id': picking.id})
+    #             move._assign_picking_post_process(new=picking)
+    #         # If this method is called in batch by a write on a one2many and
+    #         # at some point had to create a picking, some next iterations could
+    #         # try to find back the created picking. As we look for it by searching
+    #         # on some computed fields, we have to force a recompute, else the
+    #         # record won't be found.
+    #
+    #     return True
 
     def _get_new_picking_values(self):
         vals = super(StockMove, self)._get_new_picking_values()
