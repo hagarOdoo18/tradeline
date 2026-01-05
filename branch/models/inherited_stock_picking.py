ae92af9658cebc30b001fb60a5c9881512e14717
ezzat <e.mosaad@ejad.sa>
2026-01-06 01:09:06 +0200

    upload

--- branch/models/inherited_stock_picking.py
+++ branch/models/inherited_stock_picking.py
@@ -24,14 +24,11 @@ class StockPicking(models.Model):
         domain += ['|', ('branch_id', '=', False), ('branch_id', 'in', self.env.user.branch_ids.ids)]
 
         return super().search_fetch(domain, StockLocation, offset, limit, order)
-    # @api.onchange('branch_id')
-    # def _onchange_branch_id(self):
-    #     selected_brach = self.branch_id
-    #     if selected_brach:
-    #         user_id = self.env['res.users'].browse(self.env.uid)
-    #         user_branch = user_id.sudo().branch_id
-    #         if user_branch and user_branch.id != selected_brach.id:
-    #             raise UserError("Please select active branch only. Other may create the Multi branch issue. \n\ne.g: If you wish to add other branch then Switch branch from the header and set that.")
 
+    @api.model
+    def set_branch(self):
+        for rec in self:
+            if rec.location_dest_id.id:
+                rec.branch_id = rec.rec.location_det_id.warehouse_id.branch_ids.id
 
 
