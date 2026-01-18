201de18bf3712c18eab2d6e2c6feb8a9c47ddffc
Abdelrahman Gamal <abdogamal6776776@gmail.com>
2026-01-18 12:26:30 +0200

    eg_skip_validation

---
+++ eg_skip_validation/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Eg Skip Validation',
+    'version': '1.0',
+    'summary': 'Brief description of the module',
+    'description': '''
+        Detailed description of the module
+    ''',
+    'category': 'Uncategorized',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base', 'mail','account','l10n_eg_edi_eta'],
+    'data': [
+        'views/account_journal.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
