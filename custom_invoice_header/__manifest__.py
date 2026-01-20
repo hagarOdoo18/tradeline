3836cbb09fd121cbcfd168090c439fe1f6514660
ezzat <e.mosaad@ejad.sa>
2026-01-21 01:13:32 +0200

    upload

--- custom_invoice_header/__manifest__.py
+++ custom_invoice_header/__manifest__.py
@@ -4,10 +4,11 @@
     'author': 'tradeline',
     'category': 'Accounting',
     'summary': 'Customize invoice report header',
-    'depends': ['account','stock_account'],
+    'depends': ['account','stock_account','l10n_eg_edi_eta'],
     'data': [
         'views/report_invoice_header.xml',
         'views/transfer_report.xml',
+        'views/sale_order_print_payment.xml',
     ],
     'installable': True,
     'application': False,
