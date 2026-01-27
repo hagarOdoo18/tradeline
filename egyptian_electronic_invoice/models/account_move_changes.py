4b216cf798820f51349e1c6df5d490503b0039d6
ezzat <e.mosaad@ejad.sa>
2026-01-27 02:59:52 +0200

    upload

--- egyptian_electronic_invoice/models/account_move_changes.py
+++ egyptian_electronic_invoice/models/account_move_changes.py
@@ -504,7 +504,8 @@ class AccountMoveInherit(models.Model):
 				raise ValidationError(_("This product uit of measure [%s] not in Tax System unites \n\n %s" %
 				                        (uom, PROD_UNIT_TYPE)))
 			unitType = uom or "D41"
-			netTotal = sales_total_amount - discount_amount
+			# netTotal = sales_total_amount - discount_amount
+			netTotal = line.price_subtotal
 			taxable_items_lines, totalTaxableFees = line._get_taxableItems(taxes_res['taxes'])
 			totalAmount += line_price_total
 			eta_description = self.env['ir.config_parameter'].sudo().get_param('egyptian_electronic_invoice.eta_description')
@@ -520,7 +521,7 @@ class AccountMoveInherit(models.Model):
 					"salesTotal": round(sales_total_amount, 5),  # Total Quantity
 					"total": round(line_price_total, 5),
 					"valueDifference": 0.00,  # TODO::  لازم تبقى 0 دايما (خاصه بالعينات المجانيه)
-					"totalTaxableFees": totalTaxableFees,  # TODO::  In CASE of [T5:T12] must sent
+					"totalTaxableFees": 0.00,  # TODO::  In CASE of [T5:T12] must sent
 					"netTotal": round(netTotal, 5),
 					"itemsDiscount": 0.00,  # TODO::  THIS VALUE NOT USED IN ODOO خصم اضافى على المنتجات كقيمه
 					"unitValue": {
