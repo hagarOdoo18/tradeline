404ed3884fa0c63c5091e73654662fb50d92c084
ezzat <e.mosaad@ejad.sa>
2026-02-09 19:27:12 +0200

    upload

--- accounting_customization/models/account_move.py
+++ accounting_customization/models/account_move.py
@@ -89,7 +89,7 @@ class AccountMove(models.Model):
 
     def get_gift_invoice(self):
         for rec in self:
-            pos_order = self.env['pos.order'].search([('name','=',rec.invoice_origin),('state','!=','cancel')])
+            pos_order = self.env['pos.order'].search([('name','=',rec.invoice_origin),('state','=','invoiced')],limmit=1)
             if pos_order:
                 return pos_order.as_gift
             else:
