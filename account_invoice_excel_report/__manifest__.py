36a002b7278185ad14c87b4dc66c2e2ad4ddaf8e
ezzat <e.mosaad@ejad.sa>
2026-02-09 23:31:44 +0200

    upload

--- account_invoice_excel_report/__manifest__.py
+++ account_invoice_excel_report/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '18.0.1.1.0',
     'category': 'Accounting',
     'summary': 'Export invoices & payments to Excel (Odoo 18)',
-    'depends': ['account'],
+    'depends': ['account','branch'],
     'data': [
         'security/ir.model.access.csv',
         'views/invoice_excel_wizard_view.xml',
