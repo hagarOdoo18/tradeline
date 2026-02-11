89525d8388f39efce72b02e6d19a1d802684772e
ezzat <e.mosaad@ejad.sa>
2026-02-11 18:13:24 +0200

    upload

--- egyptian_electronic_invoice/models/account_move_changes.py
+++ egyptian_electronic_invoice/models/account_move_changes.py
@@ -512,7 +512,7 @@ class AccountMoveInherit(models.Model):
 				# line_price_total = round(line.price_total * currencyExchangeRate,4)
 				sales_total_amount = round(price_unit * currencyExchangeRate, 4) * quantity
 
-				line_price_total = price_unit * line.quantity * (1 - line.discount / 100) * (
+				line_price_total = line.price_unit * line.quantity * (1 - line.discount / 100) * (
 						1 + sum_line_taxes_no_deduction) * currencyExchangeRate
 			price_unit_wo_discount = line.price_unit * (1 - (line.discount / 100.0))
 			discount_percentage = line.discount if line.discount else 0.00000
