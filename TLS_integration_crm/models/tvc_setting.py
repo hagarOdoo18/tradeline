2da2eda91c20df8fac0d983e3ab19e54042fcf4a
ezzat <e.mosaad@ejad.sa>
2026-02-16 23:48:32 +0200

    upload

--- TLS_integration_crm/models/tvc_setting.py
+++ TLS_integration_crm/models/tvc_setting.py
@@ -66,81 +66,76 @@ class TVCSetting (models.Model) :
 
 
     def sent_TVC_invoice ( self ) :
-        if self._cr.dbname == "live_11nov_2024" :
+        if self._cr.dbname == "tradelinestores-production-25284095" :
             if self.from_date and self.to_date:
-                if self.offer:
-                    tvc_invoices = self.sudo ().env ['account.move'].invoice_date (
-                        [('invoice_date', '>=', self.from_date),('date_invoice', '<=', self.to_date),
-                         ('type', '=', 'out_invoice'),
-                         ('state', 'in', ['paid', 'open']),('sms_sent','=',True),
-                         ('is_tvc', '=', False),('residual_signed', '<=', 1),
-                         '|',('partner_id.customer_type', '=', 'Individual'),
-                         ('partner_id.foreigner_type', '=', 'Person'), ('team_id', '!=', 52),('offer','=',True),], order='date_invoice')
-                    self.env ['account.invoice'].post_invoiced_offer (tvc_invoices,self.offer_key)
-                    # tvc_sro_orders = self.sudo ().env ['sale.order'].search (
-                    #     [('create_date', '=', self.from_date),
-                    #      ('quotation_type', '=', 'sro'), ('state', '=', 'sale'),
-                    #      ('is_tvc', '=', False), ('user_id.name', 'ilike', 'TLS'),('offer','=',True),
-                    #      '|', ('partner_id.customer_type', '=', 'Individual'),
-                    #      ('partner_id.foreigner_type', '=', 'Person'), ('team_id', '!=', 52),('partner_id.is_exempt_select', 'not in', ['true'])], order='create_date')
-                    # self.env ['account.invoice'].post_so_offer (tvc_sro_orders,self.offer_key)
-                elif str (self.from_date) >= '2022-01-01' :
+                # if self.offer:
+                #     tvc_invoices = self.sudo ().env ['account.move'].invoice_date (
+                #         [('invoice_date', '>=', self.from_date),('date_invoice', '<=', self.to_date),
+                #          ('move_type', '=', 'out_invoice'),
+                #          ('payment_state', 'in', ['not_paid', 'paid', 'in_payment', 'partial', 'reversed']),
+                #          ('is_tvc', '=', False), ('branch_id', 'not in', '(64,90,91,93)')], order='date_invoice')
+                #     self.env ['account.invoice'].post_invoiced_offer (tvc_invoices,self.offer_key)
+                #     # tvc_sro_orders = self.sudo ().env ['sale.order'].search (
+                #     #     [('create_date', '=', self.from_date),
+                #     #      ('quotation_type', '=', 'sro'), ('state', '=', 'sale'),
+                #     #      ('is_tvc', '=', False), ('user_id.name', 'ilike', 'TLS'),('offer','=',True),
+                #     #      '|', ('partner_id.customer_type', '=', 'Individual'),
+                #     #      ('partner_id.foreigner_type', '=', 'Person'), ('team_id', '!=', 52),('partner_id.is_exempt_select', 'not in', ['true'])], order='create_date')
+                #     # self.env ['account.invoice'].post_so_offer (tvc_sro_orders,self.offer_key)
+                if str (self.from_date) >= '2022-01-01' :
                     tvc_invoices = self.sudo ().env ['account.move'].search (
-                        [('invoice_date', '>=', self.from_date),('date_invoice', '<=', self.to_date), ('type', '=', 'out_invoice'),
-                         ('state', 'in', ['paid', 'open']),
-                         ('is_tvc', '=', False), ('residual_signed','<=',1),('offer','=',False),
-                          '|',('partner_id.company_type', '=', 'person'),('team_id', '!=', 52)], order='invoice_date')
+                        [('invoice_date', '>=', self.from_date),('date_invoice', '<=', self.to_date), ('move_type', '=', 'out_invoice'), ('payment_state', 'in', ['not_paid','paid','in_payment','partial','reversed']),
+                     ('is_tvc', '=', False),('branch_id','not in','(64,90,91,93)')], order='invoice_date')
                     self.env ['account.move'].post_invoiced(tvc_invoices)
                     tvc_sro_orders = self.sudo ().env ['sale.order'].search (
-                        [('date_order', '>=', self.from_date), ('date_order', '<=', self.to_date), ('quotation_type', '=', 'sro'), ('state', '=', 'sale'),
-                         ('is_tvc', '=', False),('offer','=',False),
-                         '|',('partner_id.company_type', '=', 'person') ,('team_id', '!=', 52)], order='invoice_date')
+                        [('date_order', '>=', self.from_date), ('date_order', '<=', self.to_date), ('inv_type', '=', 'sro'), ('state', '=', 'sale'),
+                     ('is_tvc', '=', False),('branch_id','not in','(64,90,91,93)')], order='date_order')
                     self.env ['account.move'].post_so (tvc_sro_orders)
-            else:
-                if self.offer:
-                    if self.invoice_id:
-                        if self.amount:
-
-                            if self.invoice_id.type=='out_invoice':
-                                item_card = self.env ['account.move']._get_card (self.invoice_id.partner_id, self.invoice_id.date_invoice)
-
-                                tvc_dict = self.env ['account.move']._dict_invocie_offer (self.invoice_id, int ( self.amount),self.offer_key)
-                                self.env ['account.move']._post_tcv_invoice (tvc_dict, self.invoice_id, False, int ( self.amount), item_card)
-                            else:
-                                item_card = self.env ['account.move']._get_card (self.invoice_id.partner_id,
-                                                                                    self.invoice_id.date_invoice)
-
-                                tvc_dict = self.env ['account.move']._dict_invocie_offer (self.invoice_id,
-                                                                                             -1*int (self.amount),
-                                                                                             self.offer_key)
-                                self.env ['account.move']._post_tcv_invoice (tvc_dict, self.invoice_id, False,
-                                                                                -1*int (self.amount), item_card)
-                        else:
-                            tvc_invoices = self.sudo ().env ['account.move'].search (
-                                [('id', '=', self.invoice_id.id),])
-                            if tvc_invoices.type=='out_invoice':
-                                self.env ['account.move'].post_invoiced_offer (tvc_invoices,self.offer_key)
-                            else:
-                                self.env ['account.move'].post_invoiced_credit_offer (tvc_invoices, self.offer_key)
-                    elif self.order_id:
-                        if self.amount :
-                            item_card = self._get_card (self.order_id.partner_id, self.order_id.create_date)
-
-                            tvc_dict = self._dict_order_offer (self.order_id, int (self.amount ),self.offer_key)
-
-                            self._post_tcv_invoice (tvc_dict, False, self.order_id, int (self.amount), item_card)
-
-                        else:
-                            tvc_order = self.sudo ().env ['sale.order'].search (
-                                [('id', '=', self.order_id.id)])
-                            self.env ['account.move'].post_so_offer (tvc_order,self.offer_key)
-                else:
-                    tvc_invoices = self.sudo ().env ['account.move'].search (
-                        [('id', '=', self.invoice_id.id)])
-                    self.env ['account.move'].post_invoiced (tvc_invoices)
-                    tvc_order = self.sudo ().env ['sale.order'].search (
-                        [('id', '=', self.order_id.id)])
-                    self.env ['account.move'].post_so (tvc_order)
+            # else:
+            #     if self.offer:
+            #         if self.invoice_id:
+            #             if self.amount:
+            #
+            #                 if self.invoice_id.type=='out_invoice':
+            #                     item_card = self.env ['account.move']._get_card (self.invoice_id.partner_id, self.invoice_id.date_invoice)
+            #
+            #                     tvc_dict = self.env ['account.move']._dict_invocie_offer (self.invoice_id, int ( self.amount),self.offer_key)
+            #                     self.env ['account.move']._post_tcv_invoice (tvc_dict, self.invoice_id, False, int ( self.amount), item_card)
+            #                 else:
+            #                     item_card = self.env ['account.move']._get_card (self.invoice_id.partner_id,
+            #                                                                         self.invoice_id.date_invoice)
+            #
+            #                     tvc_dict = self.env ['account.move']._dict_invocie_offer (self.invoice_id,
+            #                                                                                  -1*int (self.amount),
+            #                                                                                  self.offer_key)
+            #                     self.env ['account.move']._post_tcv_invoice (tvc_dict, self.invoice_id, False,
+            #                                                                     -1*int (self.amount), item_card)
+            #             else:
+            #                 tvc_invoices = self.sudo ().env ['account.move'].search (
+            #                     [('id', '=', self.invoice_id.id),])
+            #                 if tvc_invoices.type=='out_invoice':
+            #                     self.env ['account.move'].post_invoiced_offer (tvc_invoices,self.offer_key)
+            #                 else:
+            #                     self.env ['account.move'].post_invoiced_credit_offer (tvc_invoices, self.offer_key)
+            #         elif self.order_id:
+            #             if self.amount :
+            #                 item_card = self._get_card (self.order_id.partner_id, self.order_id.create_date)
+            #
+            #                 tvc_dict = self._dict_order_offer (self.order_id, int (self.amount ),self.offer_key)
+            #
+            #                 self._post_tcv_invoice (tvc_dict, False, self.order_id, int (self.amount), item_card)
+            #
+            #             else:
+            #                 tvc_order = self.sudo ().env ['sale.order'].search (
+            #                     [('id', '=', self.order_id.id)])
+            #                 self.env ['account.move'].post_so_offer (tvc_order,self.offer_key)
+            #     else:
+            #         tvc_invoices = self.sudo ().env ['account.move'].search (
+            #             [('id', '=', self.invoice_id.id)])
+            #         self.env ['account.move'].post_invoiced (tvc_invoices)
+            #         tvc_order = self.sudo ().env ['sale.order'].search (
+            #             [('id', '=', self.order_id.id)])
+            #         self.env ['account.move'].post_so (tvc_order)
 
 
     def post_file_point( self ,invoice,InvoiceAmount,point):
