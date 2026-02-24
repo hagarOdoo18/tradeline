cdb032a8d5dea81b688c3c614320699a9fd6f119
ezzat <e.mosaad@ejad.sa>
2026-02-25 01:47:19 +0200

    upload

--- wise_pos_stock_fixed/static/src/utils.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module */
-export function getProductQty(stockType, product) {
-  const qty =
-    stockType == 'on_hand' ? product.qty_available : product.virtual_available;
-  return qty;
-}
