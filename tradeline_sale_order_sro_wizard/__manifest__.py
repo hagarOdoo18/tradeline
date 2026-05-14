3d778f8726bbe8ad142dbe310c6682f7d65b1fa1
Karim Sharaf <114778393+karimsharaf04@users.noreply.github.com>
2026-05-14 18:35:55 +0300

    Add Odoo 18 SRO sales date range wizard

---
+++ tradeline_sale_order_sro_wizard/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tradeline Sale Order SRO Wizard",
+    "version": "18.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["base", "sale_management", "accounting_customization"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/date_wizard.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
+
