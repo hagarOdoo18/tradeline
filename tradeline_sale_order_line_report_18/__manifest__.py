f1065056b30ea4333b2c02b4329fc352e2de71e6
Karim Sharaf <114778393+karimsharaf04@users.noreply.github.com>
2026-05-16 20:53:55 +0300

    Add Odoo 18 sale order line report addon aligned with Odoo 12 behavior

---
+++ tradeline_sale_order_line_report_18/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Tradeline Sale Order Line Report 18",
+    "summary": "Odoo 18 sale order line report adapted from the legacy Tradeline Odoo 12 view",
+    "version": "18.0.1.0.0",
+    "category": "Sales/Sales",
+    "author": "Tradeline",
+    "license": "LGPL-3",
+    "depends": [
+        "sale",
+        "sales_team",
+        "product",
+        "account",
+    ],
+    "data": [
+        "views/sale_order_line_report_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
