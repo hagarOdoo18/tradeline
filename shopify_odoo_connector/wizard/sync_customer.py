bdaadbc12ce2d5414bab6aec945cb10d84e4f439
e.mosad <eahmed@2p.com.sa>
2026-07-28 10:23:16 +0300

    update

--- shopify_odoo_connector/wizard/sync_customer.py
+++ shopify_odoo_connector/wizard/sync_customer.py
@@ -224,7 +224,7 @@ class SyncCustomer(models.TransientModel):
             details.
         """
         shopify_instance = instance
-        _logger.info('++++++++++customers++++++++++++++++++++',shopify_customers)
+        _logger.info(shopify_customers)
 
         for customer in shopify_customers:
             phone = customer['phone'] or ''
