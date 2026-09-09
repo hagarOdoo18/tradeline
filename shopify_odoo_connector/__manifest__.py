1df27766b5b4703b9db92a0a991bc79ec6ef52bb
e.mosad <eahmed@2p.com.sa>
2026-09-09 17:08:17 +0300

    update

--- shopify_odoo_connector/__manifest__.py
+++ shopify_odoo_connector/__manifest__.py
@@ -22,7 +22,7 @@
 ################################################################################
 {
     'name': "Shopify Odoo Connector",
-    'version': '18.0.1.0.3',
+    'version': '18.0.1.0.4',
     'summary': """Shopify Odoo Connector enables users to connect with  
      shopify to odoo and sync sale orders, customers and  products""",
     'description': """ Shopify Odoo Connector enables users to connect with 
