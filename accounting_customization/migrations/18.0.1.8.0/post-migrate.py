a19d9add98ac200c325cbebb11b24fb8a78c9eba
Karim Sharaf <114778393+karimsharaf04@users.noreply.github.com>
2026-09-10 22:40:02 +0300

    Handle locked SRO orders during tax migration

--- accounting_customization/migrations/18.0.1.8.0/post-migrate.py
+++ accounting_customization/migrations/18.0.1.8.0/post-migrate.py
@@ -1,12 +1,22 @@
-from odoo import Command, SUPERUSER_ID, api
+from psycopg2 import sql
+
+from odoo import SUPERUSER_ID, api
 
 
 def migrate(cr, version):
     """Remove taxes left on SRO order lines by earlier module versions."""
     env = api.Environment(cr, SUPERUSER_ID, {})
-    sro_lines = env['sale.order.line'].search([
-        ('order_id.inv_type', '=', 'sro'),
-        ('tax_id', '!=', False),
-    ])
-    if sro_lines:
-        sro_lines.write({'tax_id': [Command.clear()]})
+    tax_field = env['sale.order.line']._fields['tax_id']
+    cr.execute(
+        sql.SQL("""
+            DELETE FROM {relation} AS rel
+            USING sale_order_line AS line, sale_order AS sale
+            WHERE rel.{line_column} = line.id
+              AND sale.id = line.order_id
+              AND sale.inv_type = %s
+        """).format(
+            relation=sql.Identifier(tax_field.relation),
+            line_column=sql.Identifier(tax_field.column1),
+        ),
+        ('sro',),
+    )
